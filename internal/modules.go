c9c69bbb201dcfcfaa27659d7f591ceab81e3e6f
Hurt6465-ai <hurt6465@gmail.com>
2026-06-21 05:35:55 +0630

    Update modules.go

--- internal/modules.go
+++ internal/modules.go
@@ -4,6 +4,9 @@ package modules
 import (
 	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/base"
 	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/channel"
+	// chatrooms 必须放在 group 前面。开放话题房也使用 group channel_type，
+	// 先注册 chatrooms 可让 datasource 优先识别 topic room，避免走普通群好友校验。
+	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/chatrooms"
 	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/common"
 	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/file"
 	_ "github.com/TangSengDaoDao/TangSengDaoDaoServer/modules/group"
